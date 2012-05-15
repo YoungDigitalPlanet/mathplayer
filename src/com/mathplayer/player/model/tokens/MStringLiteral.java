1e401a5eefea5d8ddb1bc3550cf37df917e2e019
rrybacki <rrybacki@ydp.eu>
2012-05-15 12:08:07 +0000

    replace unsupported characters bug 62800
    git-svn-id: http://svn.intranet.ydp/ydp/projects/math-js/math-player@104112 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/com/mathplayer/player/model/tokens/MStringLiteral.java
+++ src/com/mathplayer/player/model/tokens/MStringLiteral.java
@@ -1,9 +1,18 @@
 package com.mathplayer.player.model.tokens;
 
+import com.google.gwt.user.client.Window.Navigator;
+
 public class MStringLiteral extends ContentTextTokenBase {
 
 	public MStringLiteral(String content) {
-		super(content);
+		super(replaceUnsupportedCharacters(content));
+	}
+	
+	private static String replaceUnsupportedCharacters(String c){
+		if (Navigator.getUserAgent().contains("MSIE 8.0")){
+			c = c.replace("\u211D", "R");
+		}
+		return c;
 	}
 
 	@Override
