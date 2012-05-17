182c800d40ae8e8565088cc522e016800c422560
plelakowski <plelakowski@ydp.eu>
2012-05-17 10:10:13 +0000

    math center in text bug id : 62335
    git-svn-id: http://svn.intranet.ydp/ydp/projects/math-js/math-player@104361 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/com/mathplayer/player/interaction/InteractionManager.java
+++ src/com/mathplayer/player/interaction/InteractionManager.java
@@ -1,6 +1,9 @@
 package com.mathplayer.player.interaction;
 
 import java.util.Vector;
+
+import com.google.gwt.dom.client.Style.Overflow;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -22,7 +25,7 @@ public class InteractionManager implements InteractionSocket {
 	private int actualGapHeight;
 	private int userGapWidth;
 	private int userGapHeight;
-	
+
 	public InteractionManager(Panel owner){
 		container = new AbsolutePanel();
 		container.setStyleName("math-player-container");
@@ -30,10 +33,10 @@ public class InteractionManager implements InteractionSocket {
 		canvasLayer.setStyleName("math-player-canvas");
 		uiLayer = new AbsolutePanel();
 		uiLayer.setStyleName("math-player-ui");
-		
+
 		container.add(canvasLayer, 0, 0);
 		container.add(uiLayer, 0, 0);
-		
+
 		owner.add(container);
 
 		testGap = new PanelGap();
@@ -47,14 +50,14 @@ public class InteractionManager implements InteractionSocket {
 		userGapHeight = 14;
 		actualGapWidth = 32;
 		actualGapHeight = 20;
-		
+
 	}
 
 	@Override
 	public void addGap(int x, int y) {
-		positions.add(new Point(x, y));		
+		positions.add(new Point(x, y));
 	}
-	
+
 	public void process(){
 		for (Point p : positions){
 			PanelGap pg = new PanelGap();
@@ -67,18 +70,21 @@ public class InteractionManager implements InteractionSocket {
 	@Override
 	public void setCanvas(Widget canvas, int width, int height) {
 		container.setSize(String.valueOf(width)+"px", String.valueOf(height)+"px");
+		container.getParent().setSize(String.valueOf(width)+"px", String.valueOf(height)+"px");
+		container.getElement().getStyle().setMarginTop(height/2-7, Unit.PX);
+		container.getElement().getStyle().setOverflow(Overflow.VISIBLE);
 		canvasLayer.setSize(String.valueOf(width)+"px", String.valueOf(height)+"px");
 		uiLayer.setSize(String.valueOf(width)+"px", String.valueOf(height)+"px");
 		canvasLayer.add(canvas, 0, 0);
-		
+
 	}
-	
+
 	public void removeTextBox() {
 		actualGapWidth = testGap.getGap().getOffsetWidth();
 		actualGapHeight = testGap.getGap().getOffsetHeight();
 		RootPanel.get().remove(testGap);
 	}
-	
+
 	public void setTextBoxHeight(int h){
 		if (h >= 0){
 			userGapHeight = h;
@@ -90,7 +96,7 @@ public class InteractionManager implements InteractionSocket {
 	public int getTextBoxHeight() {
 		return actualGapHeight;
 	}
-	
+
 	public void setTextBoxWidth(int w){
 		if (w >= 0){
 			userGapWidth = w;
@@ -102,25 +108,25 @@ public class InteractionManager implements InteractionSocket {
 	public int getTextBoxWidth() {
 		return actualGapWidth;
 	}
-	
+
 	public int getGapsCount(){
 		return gaps.size();
 	}
-	
+
 	public TextBox getGapAt(int i){
 		if (i < gaps.size())
 			return gaps.get(i).getGap();
 		return null;
 	}
-	
+
 	public void markGap(int index, boolean correct, boolean wrong){
 		if (index < gaps.size())
-			gaps.get(index).mark(correct, wrong);		
+			gaps.get(index).mark(correct, wrong);
 	}
-	
+
 	public void unmarkGap(int index){
 		if (index < gaps.size())
-			gaps.get(index).unmark();		
+			gaps.get(index).unmark();
 	}
 
 	@Override
