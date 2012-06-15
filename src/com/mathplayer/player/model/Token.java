7347c9bf4507f9fc746c1df0c42fa6c4b8cd1d33
plelakowski <plelakowski@ydp.eu>
2012-06-15 15:23:16 +0000

    text position in android browser bug id: 65202
    git-svn-id: http://svn.intranet.ydp/ydp/projects/math-js/math-player@107578 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/com/mathplayer/player/model/Token.java
+++ src/com/mathplayer/player/model/Token.java
@@ -7,6 +7,9 @@ import com.mathplayer.player.geom.Size;
 import com.mathplayer.player.interaction.InteractionSocket;
 import com.mathplayer.player.utils.BrowserUtils;
 
+import eu.ydp.gwtutil.client.util.UserAgentChecker;
+import static eu.ydp.gwtutil.client.util.UserAgentChecker.MobileUserAgent.*;
+
 import gwt.g2d.client.graphics.Surface;
 
 public abstract class Token {
@@ -15,51 +18,58 @@ public abstract class Token {
 	protected Size size;
 	protected Area exactArea;
 
-	protected static final double TEXT_OFFSET = BrowserUtils.getUserAgent().toLowerCase().contains("msie") ? 0.91d : 0.72d;
-	
-	
-	public void setFont(Font font){
+	protected static double TEXT_OFFSET = BrowserUtils.getUserAgent().toLowerCase().contains("msie") ? 0.91d : 0.72d;
+	static {
+		if (UserAgentChecker.isMobileUserAgent(ANDROID23, ANDROID3, ANDROID321, ANDROID4)) {
+			TEXT_OFFSET = 0.81d;
+		}
+	}
+
+	public void setFont(Font font) {
 		this.font = font;
 	}
-	
-	public void render(Surface canvas, Area area, InteractionSocket socket){
+
+	public void render(Surface canvas, Area area, InteractionSocket socket) {
 		findExactArea(area);
 	}
+
 	public abstract Size measure(InteractionSocket socket);
+
 	public abstract String toString();
+
 	public abstract String toMathML();
-	
-	public Surface createCanvas(){
+
+	public Surface createCanvas() {
 		Surface canvas = new Surface();
 		RootPanel.get().add(canvas);
 		return canvas;
 	}
-	
-	public void removeCanvas(Surface canvas){
+
+	public void removeCanvas(Surface canvas) {
 		RootPanel.get().remove(canvas);
 	}
 
-	public Area getExactArea(){
+	public Area getExactArea() {
 		return exactArea.clone();
 	}
-	
-	protected void findExactArea(Area area){
+
+	protected void findExactArea(Area area) {
 		double heightSurplus = area.middleLine - size.middleLine;
 		if (area.y + heightSurplus < 0)
 			heightSurplus = -area.y;
-		exactArea = new Area(area.x + (area.width-size.width)/2, area.y + heightSurplus, size);
+		exactArea = new Area(area.x + (area.width - size.width) / 2, area.y + heightSurplus, size);
 	}
 
-	public double getTextOffset(){
+	public double getTextOffset() {
 		return font.size * TEXT_OFFSET;
 	}
-	
-	public double getTextOffset(int fontSize){
+
+	public double getTextOffset(int fontSize) {
 		return fontSize * TEXT_OFFSET;
 	}
-	
-	public void reset(){
+
+	public void reset() {
 		size = null;
 	}
-	
+
 }
